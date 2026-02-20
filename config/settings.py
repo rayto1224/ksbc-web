e4c792ea4c8715ca4589ff53b0a751df91201384
rayto1224 <rayto1224@gmail.com>
2026-02-21 00:44:34 +0800

    update settings.py again 2

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["ksbc-web.onrender.com", "127.0.0.1", "localhost"]
 
@@ -91,7 +91,7 @@ DATABASES = {
         "NAME": "church",
         "USER": "postgres",
         "PASSWORD": "0070",
-        "HOST": "localhost",
+        "HOST": "ksbc-web.onrender.com",
     }
 }
 
