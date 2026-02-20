e8eb14e7cf294b60b13ab96f12c13a2c80d266fb
rayto1224 <rayto1224@gmail.com>
2026-02-21 00:13:11 +0800

    update settings.py again

--- config/settings.py
+++ config/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "ksbc-web.onrender.com"]
+ALLOWED_HOSTS = ["ksbc-web.onrender.com", "127.0.0.1", "localhost"]
 
 
 # Application definition
